072000a78c1cd43eb669927c88c1710898bdfa0f
YeBowe24 <bowen.24@intl.zju.edu.cn>
2025-03-19 08:39:12 +0100

    P5new

---
+++ Practical5/favors.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+dic = {'JavaScript':62.3,'HTML':52.9,'Python':51,'SQL':51,'TypeScript':38.5}
+df = pd.DataFrame(list(dic.items()), columns=['Language', 'Percentage'])
+dfsort = df.sort_values(by='Percentage',ascending=False)
+#dfsort.plot.bar(x='Language', y='Percentage')
+#plt.show()
+df1 = df[df.Language == 'Python']
+df2 = df[df.Language == 'SQL']
+df3 = df[df['Percentage'] >= 50]
+print(df3)
