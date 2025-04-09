66c7272f34f75f00263d14d475c8bef57a38e241
YeBowe24 <bowen.24@intl.zju.edu.cn>
2025-04-09 08:13:12 +0100

    Added examples in the  Restriction code

--- Practical8/Restriction_enzyme_cut_sites.py
+++ Practical8/Restriction_enzyme_cut_sites.py
@@ -15,9 +15,20 @@ while True :
         break
     print('Illegal nucleotide, please input the sequence that only contains "ACGT"!')
 
-#ide = 
 place = re.search(fnd,seq)
 if place :
     print('The restriction digest start at the %dth nucleotide.'%(place.start()))
 else :
     print('The sequence you want to search is not in the DNA sequence!')
+
+'''
+Here are some examples:
+seq1 = 'UGCGAUGAAGAGUUCCGACU'
+seq2 = 'TTCTTATAGTATCGTACCTGGTGAGCCCCG'
+fnd0 = 'UAAU'
+fnd1 = 'AGTTTT'
+fnd2 = 'TCGTA'
+if you input seq1 or fnd0, then the code will output 'Illegal nucleotide, please input the sequence that only contains "ACGT"!'
+if you input seq2 and fnd1, then the code will output 'The sequence you want to search is not in the DNA sequence!'
+if you input seq2 and fnd2, then the code will output 'The restriction digest start at the 11th nucleotide.'
+'''
