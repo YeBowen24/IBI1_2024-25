fd4c93a422fb1c6bf7c318b0daf8ba694f99f07e
bowen <bowen.24@intl.zju.edu.cn>
2025-04-22 21:43:20 +0800

    Added some pseudocode into the code

--- Practical8/Restriction_enzyme_cut_sites.py
+++ Practical8/Restriction_enzyme_cut_sites.py
@@ -10,12 +10,12 @@ def errfnd(seq):
 while True :
     seq = input('Input the DNA sequence HERE: ')
     fnd = input('Input the restriction digest you want to find:')
-    err = errfnd(seq) and errfnd(fnd)
+    err = errfnd(seq) and errfnd(fnd) #check if the seq have illegal nucleotide
     if not err :
         break
-    print('Illegal nucleotide, please input the sequence that only contains "ACGT"!')
+    print('Illegal nucleotide, please input the sequence that only contains "ACGT"!') #if there is a illegal nucleotide in the seq, then the code will print this sentence and ask for another input
 
-place = re.search(fnd,seq)
+place = re.search(fnd,seq) #search for the beginning of the restriction digest
 if place :
     print('The restriction digest start at the %dth nucleotide.'%(place.start()))
 else :
