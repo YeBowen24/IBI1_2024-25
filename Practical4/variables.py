909ac70cfb572a7d116303c927ac3840693b0c78
bowen <bowen.24@intl.zju.edu.cn>
2025-05-27 17:32:52 +0800

    Feedback--Add variable W and the compare between c&f

--- Practical4/variables.py
+++ Practical4/variables.py
@@ -4,4 +4,14 @@ c = a + b
 d = 60 + 30
 e = 5
 f = d + e
+if f > c:
+    print('f is greater than c')
+else: 
+    print('c is greater than f')
+
+print('IS c greater than e? ',end='')
 print(c>e)
+
+X = True ; Y = False
+W = [X and Y, not X and Y, X and not Y, not X and not Y, X or Y, not X or Y, X or not Y, not X or not Y]
+print(W)
