407cece44ef0774e11f8d28468fe5eb333f9a0dd
bowen <bowen.24@intl.zju.edu.cn>
2025-05-27 17:47:19 +0800

    Feedback--Added the pseudocode in BMI_calculator.py

--- Practical4/BMI_calculator.py
+++ Practical4/BMI_calculator.py
@@ -1,3 +1,11 @@
+'''
+pseudocode
+Prompt the user to input the weight (unit: kilograms), and convert it to an integer to store in the variable "weight"
+Prompt the user to enter the height (unit: meters), and convert it to a floating-point number to store in the variable "height"
+To calculate the BMI value, the formula is: BMI = weight/(height)^2, and keep the result to two decimal places
+Print the BMI value of the user in the format of "Your BMI is followed by the calculated BMI value".
+'''
+
 weight = int(input('input your weight here(kg)'))
 height = float(input('input your height here(m)'))
 BMI = round(weight/height**2,2)
