e629aae4b86e2f15c385bcf6c949db756160e7a7
bowen <bowen.24@intl.zju.edu.cn>
2025-05-27 18:36:16 +0800

    Feedback--Added the pseudocodes

--- Practical5/favors.py
+++ Practical5/favors.py
@@ -1,5 +1,29 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+
+'''
+pseudocode
+
+Create a dictionary with programming languages as keys and their corresponding percentages as values, then print the dictionary
+ 
+Convert the dictionary to a pandas DataFrame with column names 'Language' and 'Percentage'
+ 
+Sort the DataFrame by the 'Percentage' column in descending order
+ 
+Plot a bar chart, set the figure size, with x-axis as 'Language' and y-axis as 'Percentage'
+ 
+Add x-axis label as 'Language Type', y-axis label as 'Number of Users', include a legend, and finally display the chart
+ 
+Start an infinite loop:
+Prompt the user to input the programming language they want to search
+If the input language exists in the dictionary:
+Get the corresponding percentage and store it in variable 'out'
+Break the loop
+Else:
+Notify the user that the language doesn't exist in the dictionary and request correct input
+Print the variable 'out' (the percentage found for the user's query)
+'''
+
 dic = {'JavaScript':62.3,'HTML':52.9,'Python':51,'SQL':51,'TypeScript':38.5} 
 print(dic)
 df = pd.DataFrame(list(dic.items()), columns=['Language', 'Percentage']) #Use dataframe to draw the plot
