5ca369c80c8501c1cf21467b5f38b5cfc8790526
bowen <bowen.24@intl.zju.edu.cn>
2025-05-27 17:39:20 +0800

    Feedback--Now the code can print all the triangles

--- Practical4/triangle.py
+++ Practical4/triangle.py
@@ -1,5 +1,10 @@
-time = int(input())
-num = 0
-for i in range(1,time+1): #range(2,1)  --- []
-    num += i
-print (num)
+def tri_sum(t):
+    num = 0
+    for i in range(1,t+1): #range(2,1)  --- []
+        num += i
+    print (num,end = '  ')
+
+time = int(input('Input time loop: '))
+
+for i in range(1,time+1):
+    tri_sum(i)
