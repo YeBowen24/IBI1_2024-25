70428680937a2229f03b7d1714e4ccbf1792f4ff
bowen <bowen.24@intl.zju.edu.cn>
2025-05-27 17:45:21 +0800

    Feedback--Added the pseudocode in tri.py

--- Practical4/triangle.py
+++ Practical4/triangle.py
@@ -1,3 +1,13 @@
+'''
+Get the input 'time' value representing the number of triangular numbers to generate
+
+Initialize a variable to store the triangular number value
+
+Loop from 1 to time (inclusive):
+    Add the current loop index value to the triangular number storage variable
+    Print the calculated current triangular number value followed by a space
+'''
+
 def tri_sum(t):
     num = 0
     for i in range(1,t+1): #range(2,1)  --- []
